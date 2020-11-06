b53f5a992ada82113014ab61542311257dab2549
ilezus.dev <ilezus.dev@gmail.com>
2020-11-07 00:14:57 +0300

    Some fixes

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ import Header from "./components/Header";
 
 import { AppContext } from "./context";
 import { getTheme, setTheme } from "./helpers/theme";
-import { setLanguage } from "./localization";
 
 const App = () => {
   const [message, addMessage] = useState(null);
   const [theme, updateTheme] = useState(getTheme());
-  const [, updateLanguage] = useState();
 
   const toggleTheme = (theme) => {
     setTheme(theme);
     updateTheme(theme);
-  };
-
-  const toggleLanguage = (language) => {
-    setLanguage(language);
-    updateLanguage(language);
   };
 
   return (
@@ -28,7 +21,6 @@ const App = () => {
       value={{
         theme,
         toggleTheme,
-        toggleLanguage,
       }}
     >
       <div className="container">
