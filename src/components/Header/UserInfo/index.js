c1cabc87cb8ee96c61acaff29b2c45ffb024e17e
ilezus.dev <ilezus.dev@gmail.com>
2020-11-08 09:17:57 +0300

    Some fixes

---
+++ src/components/Header/UserInfo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const UserInfo = () => (
+  <div className="user-info">
+    <div className="name">Ilezus Dev</div>
+    <div className="status">
+      <span className="circle" />В сети
+    </div>
+  </div>
+);
+
+export default UserInfo;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,14 @@
 import React from "react";
+import UserInfo from "./UserInfo";
 import Theme from "./Theme";
+
 import "./styles.css";
 
 const Header = () => {
   return (
     <div className="header">
+      <UserInfo />
       <Theme />
-      <div className="user">
-        <div className="name">Ilezus Dev</div>
-        <div className="status">
-          <span className="circle" />В сети
-        </div>
-      </div>
     </div>
   );
 };
