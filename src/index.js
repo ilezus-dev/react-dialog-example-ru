fdad1563b062939f134d413d119a8c0ab57e32a4
ilezus.dev <ilezus.dev@gmail.com>
2020-11-13 07:20:41 +0300

    Some fixes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import WebFontLoader from "webfontloader";
 import dayjs from "dayjs";
 
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 import "dayjs/locale/ru";
 import "./css/index.css";
@@ -23,8 +22,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
